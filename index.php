5f2d63b07b74ae68a5adc3fd3ea43dc61aaf9c2b
Dennis Gokman <dennis.gokman@gmail.com>
2013-09-13 23:33:29 -0400

    Adding main index page

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-Welcome to P1
+<!-- about me --> <!DOCTYPE html PUBLIC "-//W3C//DTD HTML
+4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
+<link rel="stylesheet" type="text/css" href="style.css" />
+<head>
+ <title>About Me</title> 
+</head> 
+  <body> 
+   <h1>Dennis Gokman</h1>
+      <p>Hello All. I am 30 years old and work in mobile advertising. I grew up in suburban NYC and moved to Boston 3 years ago to further my career. In my free time, I enjoy watching movies, playing basketball, and eating ice cream. I am currently wrapped up in the TV show 'Breaking Bad' and cannot wait to see how it ends. I am hoping to leverage this class and others in order to transition into development at my company</p>
+    <h2>Code Editor</h2>
+      <p>I am using TextWrangler to design this page</p>
+   </body>
