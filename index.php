85ba9de30e6907719837404d95c54367108ead30
dennis.gokman@gmail.com <dennis.gokman@gmail.com>
2013-09-17 10:31:37 -0400

    Changing index file 2

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-<!-- about me --> <!DOCTYPE html PUBLIC "-//W3C//DTD HTML
-4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
+<!DOCTYPE html>
+<html>
 <link rel="stylesheet" type="text/css" href="style.css" />
-<head>
- <title>About Me</title> 
-</head> 
-  <body>
-   <h1>Dennis Gokman</h1>
-      <p>Hello All. I am 30 years old and work in mobile advertising. I grew up in suburban NYC and moved to Boston 3 years ago to further my career. In my free time, I enjoy watching movies, playing basketball, and eating ice cream. I am currently wrapped up in the TV show 'Breaking Bad' and cannot wait to see how it ends. I am hoping to leverage this class and others in order to transition into development at my company</p>
-    <h2>Code Editor</h2>
-      <p>I am using XCode to design this page</p>
-  </body>
+<title>About Me</title>
+</head>
+ <body>
+  <h1>Dennis Gokman</h1>
+   <p>Hello All. I am 30 years old and work in mobile advertising. I grew up in suburban NYC and moved to Boston 3 years ago to further my career. In my free time, I enjoy watching movies, playing basketball, and eating ice cream. I am currently wrapped up in the TV show 'Breaking Bad' and cannot wait to see how it ends. I am hoping to leverage this class and others in order to transition into development at my company</p>
+  <h2>Code Editor</h2>
+   <p>I am using XCode to design this page</p>
+ </body>
+</html>
+
